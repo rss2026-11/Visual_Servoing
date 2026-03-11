086c9c5a04f509d22ba4dfc41f78a885a0fc574c
Tyler <tylergproctor@gmail.com>
2026-03-11 12:30:26 -0400

    Finished first version

--- visual_servoing/visual_servoing/parking_controller.py
+++ visual_servoing/visual_servoing/parking_controller.py
@@ -3,6 +3,7 @@
 import rclpy
 from rclpy.node import Node
 import numpy as np
+import math
 
 from vs_msgs.msg import ConeLocation, ParkingError
 from ackermann_msgs.msg import AckermannDriveStamped
@@ -27,11 +28,15 @@ class ParkingController(Node):
         self.create_subscription(
             ConeLocation, "/relative_cone", self.relative_cone_callback, 1)
 
-        self.parking_distance = .75  # meters; try playing with this number!
+        self.parking_distance = 1.5  # meters; try playing with this number!
         self.relative_x = 0
         self.relative_y = 0
 
         self.get_logger().info("Parking Controller Initialized")
+
+        self.distance = 0.0
+        self.speed = 0.0
+        self.steering_angle = 0.0
 
     def relative_cone_callback(self, msg):
         self.relative_x = msg.x_pos
@@ -42,6 +47,26 @@ class ParkingController(Node):
 
         # YOUR CODE HERE
         # Use relative position and your control law to set drive_cmd
+
+        self.distance = math.hypot(self.relative_x, self.relative_y)
+        angle = math.atan2(self.relative_y, self.relative_x)
+
+        slow_threshold = 2.0
+
+        stop_distance = self.distance - self.parking_distance
+        self.speed = np.clip(stop_distance / slow_threshold, -1.0, 1.0)
+
+        if self.speed < 0:
+            steering_angle = -angle
+        else:
+            steering_angle = angle
+            
+        self.steering_angle = np.clip(steering_angle, -4.0, 4.0)
+            
+
+        drive_cmd.drive.steering_angle = self.steering_angle
+        drive_cmd.drive.speed = self.speed
+        drive_cmd.drive.steering_angle_velocity = 0.0 
 
         #################################
 
@@ -60,6 +85,10 @@ class ParkingController(Node):
         # YOUR CODE HERE
         # Populate error_msg with relative_x, relative_y, sqrt(x^2+y^2)
 
+        error_msg.x_error = self.relative_x
+        error_msg.y_error = self.relative_y
+
+        error_msg.distance_error = self.distance
         #################################
 
         self.error_pub.publish(error_msg)
